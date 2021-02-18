453a724859de7b6dd9c980c4d1e857af140f323b
nikhil153 <nikhil153@gmail.com>
2021-02-18 17:00:30 -0500

    added kaggle MRI dataset

--- ml/data.py
+++ ml/data.py
@@ -1,6 +1,10 @@
+import numpy as np
 import torch
 import torchvision
 import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from kaggle_dataset import BrainSegmentationDataset as Dataset
+from kaggle_transform import custom_transforms
 
 
 def get_cifar10_dataset(data_path, batch_size, transform=True):
@@ -9,7 +13,7 @@ def get_cifar10_dataset(data_path, batch_size, transform=True):
             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
     trainset = torchvision.datasets.CIFAR10(root=data_path, train=True,
-                                            download=True, transform=transform)
+                                            download=False, transform=transform)
     train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                             shuffle=True, num_workers=2)
 
@@ -19,3 +23,38 @@ def get_cifar10_dataset(data_path, batch_size, transform=True):
                                         shuffle=False, num_workers=2)
 
     return train_loader, test_loader
+
+def get_kaggle_dataset(data_path,image_size=256,aug_scale=0.05,aug_angle=15,batch_size=4,workers=4):
+    dataset_train = Dataset(
+        images_dir = data_path,
+        subset = "train",
+        transform = custom_transforms(scale=aug_scale, angle=aug_angle, flip_prob=0.5),
+    )
+
+    dataset_test = Dataset(
+        images_dir = data_path,
+        subset = "validation",
+        image_size = image_size,
+        random_sampling = False,
+    )
+
+    def worker_init(worker_id):
+        np.random.seed(42 + worker_id)
+
+    loader_train = torch.utils.data.DataLoader(
+        dataset_train,
+        batch_size=batch_size,
+        shuffle=True,
+        drop_last=True,
+        num_workers= workers,
+        worker_init_fn=worker_init,
+    )
+    loader_test = torch.utils.data.DataLoader(
+        dataset_test,
+        batch_size= batch_size,
+        drop_last=False,
+        num_workers= workers,
+        worker_init_fn=worker_init,
+    )
+
+    return loader_train, loader_test
--- ml/run_experiment.py
+++ ml/run_experiment.py
@@ -2,7 +2,7 @@ import time
 import datetime
 import os
 import pandas as pd
-
+import numpy as np
 import torch
 import torchvision
 import torchvision.transforms as transforms
@@ -15,18 +15,21 @@ from ptflops import get_model_complexity_info
 from pyJoules.energy_meter import EnergyContext
 from pyJoules.handler.pandas_handler import PandasHandler
 
+from loss import DiceLoss
 from data import *
 from model import *
 
-experiment_name = 'Exp_pytorch_cifar'
-data_path = './data/'
+experiment_name = 'Exp_pytorch_kaggle' #Exp_pytorch_cifar
+data_path = '../datasets/kaggle_3m_small' #'../datasets/cifar10'
 
-model_name = 'ResNet_1'
+dataset_name = 'kaggle' #'cifar'
+model_name = 'unet' #'ResNet_1'
+loss_type = 'dice'
 optimizer_name = 'adam'
-n_epochs = 5
+n_epochs = 1
 batch_size = 4
 monitor_joules = True
-monitor_interval = 2000
+monitor_interval = 50 #2000
 
 output_dir = '../results/'
 
@@ -62,18 +65,27 @@ def main():
     epoch_csv = '{}/epoch.csv'.format(experiment_dir)
 
     # data
-    train_loader, test_loader = get_cifar10_dataset(data_path, batch_size)
+    if dataset_name == 'cifar':
+        train_loader, test_loader = get_cifar10_dataset(data_path, batch_size)
+    elif dataset_name == 'kaggle':
+        train_loader, test_loader = get_kaggle_dataset(data_path)
+    else:
+        print('Unknown dataset: {}'.format(dataset_name))
+
     dataiter = iter(test_loader)
     images, labels = dataiter.next()
     _,n_channels,input_size,_ = images.shape
     
+    print('input size: {}'.format([input_size,input_size,n_channels]))
     # model definition
+    print('Using {} model'.format(model_name))
     model_path = '{}/{}.pth'.format(experiment_dir,model_name)
 
     if model_name == 'ResNet_1':
         model = ResNet_1(device, ResidualBlock, [2, 2, 2]).to(device)
-    elif model_name == 'ResNet_2':
-        model = ResNet_2(device, ResidualBlock, [2, 2, 2]).to(device)
+    elif model_name == 'unet':
+        model = torch.hub.load('mateuszbuda/brain-segmentation-pytorch', 'unet',
+            in_channels=3, out_channels=1, init_features=32, pretrained=False)
     else:
         print('Unknown model: {}'.format(model_name))
 
@@ -85,7 +97,13 @@ def main():
     exp_df.loc[0] = [experiment_name, device, model_name, macs, params, n_epochs, batch_size, optimizer_name]
 
     # optimizer
-    criterion = nn.CrossEntropyLoss()
+
+    if loss_type == 'dice':
+        criterion = DiceLoss()
+    elif loss_type == 'cross-entropy':
+        criterion = nn.CrossEntropyLoss()
+    else:
+        criterion = nn.CrossEntropyLoss()
 
     if optimizer_name == 'SGD':
         optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
@@ -94,7 +112,9 @@ def main():
 
     
     # train
-    print('Begin training (n_epochs={}, batch_size={})'.format(n_epochs,batch_size))
+    tic_time = datetime.datetime.now()
+
+    print('\nBegin training at {} (n_epochs={}, batch_size={})'.format(tic_time, n_epochs,batch_size))
     
     # train start time
     train_start_time = time.time()
@@ -131,8 +151,8 @@ def main():
 
                 # print statistics
                 running_loss += loss.item()
-                avg_loss = running_loss / 2000
-                print('epoch:{}, iter:{}, loss: {:3.2f}'.format(epoch + 1, i + 1, avg_loss))
+                avg_loss = running_loss / monitor_interval
+                print('epoch:{}, iter:{}, loss: {:4.3f}'.format(epoch + 1, i + 1, avg_loss))
                 running_loss = 0.0
 
             else:
@@ -149,7 +169,8 @@ def main():
     # train end time
     train_end_time = time.time()
     train_compute_time = (train_end_time - train_start_time)/60.0
-    print('\nFinished Training at {}, compute time:{}'.format(train_end_time,train_compute_time))
+    toc_time = datetime.datetime.now()
+    print('\nFinished training at {}, compute time:{}'.format(toc_time,train_compute_time))
 
     # test the model
     # test start time
@@ -157,24 +178,42 @@ def main():
     print('Evaluating on test set')
     model.eval()
     with torch.no_grad():
-        correct = 0
-        total = 0
-        for images, labels in test_loader:
-            images = images.to(device)
-            labels = labels.to(device)
-            outputs = model(images)
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
+        if loss_type == 'cross-entropy':
+            correct = 0
+            total = 0
+            for images, labels in test_loader:
+                images = images.to(device)
+                labels = labels.to(device)
+                outputs = model(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
 
-        test_perf = 100 * correct / total
-        print('Accuracy of the model on the test images: {} %'.format(test_perf))
+            test_perf = 100 * correct / total
+            print('Accuracy of the model on the test images: {} %'.format(test_perf))
     
+        elif loss_type == 'dice':
+            running_loss = []
+            for images, labels in test_loader:
+                images = images.to(device)
+                labels = labels.to(device)
+                outputs = model(images)
+                loss = criterion(outputs, labels)
+                running_loss.append(loss.item())
+
+            test_perf = 100 * (1 - np.mean(running_loss))
+            print('Dice performance (%) of the model on the test images: {}'.format(test_perf))
+
+        else:
+            print('unknown loss type: {}'.format(loss_type))
+
     exp_df['test_perf'] = test_perf
 
     # test end time
     test_end_time = time.time()
     test_compute_time = (test_end_time - test_start_time)/60.0
+    toc_time = datetime.datetime.now()
+    print('\nFinished testing at {}, compute time:{}'.format(toc_time,test_compute_time))
 
     # experiment end time
     exp_end_time = time.time()
@@ -196,7 +235,8 @@ def main():
     print('Saving experiment config df at: {}'.format(exp_csv))
     exp_df.to_csv(exp_csv)
 
-    print('\nFinished experiment {} at {}, compute time: {}'.format(experiment_name, exp_end_time, exp_compute_time))
+    toc_time = datetime.datetime.now()
+    print('\nFinished experiment {} at {}, compute time: {}'.format(experiment_name, toc_time, exp_compute_time))
 
 
 if __name__=='__main__':
