717df5be858d3313b3450870782407b3f83a52b8
nikhil153 <nikhil153@gmail.com>
2021-05-12 15:09:54 -0400

    updated run scripts to include run_id

--- preproc_pipeline_experiments/run_nipype_reconall_slurm.sh
+++ preproc_pipeline_experiments/run_nipype_reconall_slurm.sh
@@ -2,13 +2,16 @@
 #SBATCH --account=def-jbpoline
 #SBATCH --nodes 1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=8G       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
+#SBATCH --mem=16G       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
 #SBATCH --time=0-23:00
 #SBATCH --output=logs/%N-%j.out
 #SBATCH --array=1-73
 
+
 echo "Starting task $SLURM_ARRAY_TASK_ID"
-SUBJECT_ID=$(sed -n "${SLURM_ARRAY_TASK_ID}p" subject_ids.txt)
+subject_id=$(sed -n "${SLURM_ARRAY_TASK_ID}p" subject_ids.txt)
+run_id=$1
+
 module load singularity/3.6
 
-singularity exec --overlay /project/rpp-aevans-ab/neurohub/ukbb/imaging/neurohub_ukbb_t1_ses2_0_bids.squashfs:ro ../../FreeSurfer_tracker.simg ./run_nipype.sh sub-$SUBJECT_ID
+singularity exec --overlay /project/rpp-aevans-ab/neurohub/ukbb/imaging/neurohub_ukbb_t1_ses2_0_bids.squashfs:ro ../../FreeSurfer_tracker.simg ./run_nipype_reconall.sh sub-$subject_id $run_id
